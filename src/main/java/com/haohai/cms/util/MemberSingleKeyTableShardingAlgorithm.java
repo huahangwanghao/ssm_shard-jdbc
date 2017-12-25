7e1c1cd79d7337354f893add44b771fda0f22649
wanghao2 <wanghao2@gomeholdings.com>
2017-12-25 19:03:48 +0800

    现在是有问题的

--- src/main/java/com/haohai/cms/util/MemberSingleKeyTableShardingAlgorithm.java
+++ src/main/java/com/haohai/cms/util/MemberSingleKeyTableShardingAlgorithm.java
@@ -1,28 +1,38 @@
+/*
 package com.haohai.cms.util;
 
+*/
 /**
  * Created by Administrator on 2017/12/25.
- */
+ *//*
 
-import com.dangdang.ddframe.rdb.sharding.api.ShardingValue;
-import com.dangdang.ddframe.rdb.sharding.api.strategy.table.SingleKeyTableShardingAlgorithm;
+
 import com.google.common.collect.Range;
+import io.shardingjdbc.core.api.algorithm.sharding.RangeShardingValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.LinkedHashSet;
 
+*/
 /**
  * 分表逻辑
  * @author zhangwentao
  *
- */
-public class MemberSingleKeyTableShardingAlgorithm implements SingleKeyTableShardingAlgorithm<Integer> {
+ *//*
+
+public class MemberSingleKeyTableShardingAlgorithm implements io.shardingjdbc.core.api.algorithm.sharding.standard.RangeShardingAlgorithm<Integer> {
     private static final Logger logger = LoggerFactory.getLogger(MemberSingleKeyTableShardingAlgorithm.class);
-    /**
+   
+   
+   
+   
+    */
+/**
      * sql between 规则
-     */
+     *//*
+
     public Collection<String> doBetweenSharding(Collection<String> tableNames, ShardingValue<Integer> shardingValue) {
         logger.info("sql between 规则--->tableNames:"+tableNames+",shardingValue="+shardingValue);
         Collection<String> result = new LinkedHashSet<String>(tableNames.size());
@@ -39,9 +49,11 @@ public class MemberSingleKeyTableShardingAlgorithm implements SingleKeyTableShar
         return result;
     }
 
-    /**
+    */
+/**
      * sql == 规则
-     */
+     *//*
+
     public String doEqualSharding(Collection<String> tableNames, ShardingValue<Integer> shardingValue) {
         logger.info("sql == 规则--->tableNames:"+tableNames+",shardingValue="+shardingValue);
         Integer modValue = shardingValue.getValue() % 2;
@@ -52,7 +64,8 @@ public class MemberSingleKeyTableShardingAlgorithm implements SingleKeyTableShar
         }
         throw new IllegalArgumentException();
 
-        /****
+        */
+/****
          * Long modValue = shardingValue.getValue() % 100;
          String modStr = modValue < 10 ? "0" + modValue : modValue.toString();
          for (String each : tableNames) {
@@ -61,12 +74,15 @@ public class MemberSingleKeyTableShardingAlgorithm implements SingleKeyTableShar
          }
          }
          throw new IllegalArgumentException();
-         */
+         *//*
+
     }
 
-    /**
+    */
+/**
      * sql in 规则
-     */
+     *//*
+
     public Collection<String> doInSharding(Collection<String> tableNames, ShardingValue<Integer> shardingValue) {
         logger.info("sql in 规则--->tableNames:"+tableNames+",shardingValue="+shardingValue);
         Collection<String> result = new LinkedHashSet<String>(tableNames.size());
@@ -82,4 +98,16 @@ public class MemberSingleKeyTableShardingAlgorithm implements SingleKeyTableShar
         return result;
     }
 
-}
+    @Override
+    public Collection<String> doSharding(Collection<String> collection, RangeShardingValue<Integer> rangeShardingValue) {
+        
+        logger.info("sql == 规则--->tableNames:"+tableNames+",shardingValue="+shardingValue);
+        Integer modValue = shardingValue.getValue() % 2;
+        for (String each : tableNames) {
+            if (each.endsWith("_"+modValue)) {
+                return each;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}*/
